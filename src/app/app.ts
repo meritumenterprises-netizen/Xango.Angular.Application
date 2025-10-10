d213de7064b4ac85b09369b55216ea0f52d4c4b2
meritumenterprises-netizen <meritumenterprises@gmail.com>
2025-10-11 00:26:17 +0200

    Initial page split into Header, Container and Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header';
+import { ContainerComponent } from "./container/container";
+import { FooterComponent } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ContainerComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: "./app.css"
 })
 export class App {
   protected readonly title = signal('Xango.Angular.Application');
---
+++ src/app/container/container.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './container.html',
+  styleUrl: './container.css'
+})
+export class ContainerComponent {
+
+}
